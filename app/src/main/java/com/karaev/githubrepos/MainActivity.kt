62ed22743a90bc74d32bbeb693854e6e614fce96
shamil <shomaldinho@gmail.com>
2023-02-15 17:32:16 +0300

    Добавление комментариев в про MAinActivity

--- app/src/main/java/com/karaev/githubrepos/MainActivity.kt
+++ app/src/main/java/com/karaev/githubrepos/MainActivity.kt
@@ -6,36 +6,38 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.karaev.githubrepos.fragments.RepositoriesListFragment
 
+//Главный экран приложения
 class MainActivity : AppCompatActivity() {
-
+    // Вызывется при создании Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("MainActivity","Был вызван onStart")
+        Log.d("MainActivity", "Был вызван onStart")
         setContentView(R.layout.activity_main)
         navigateRepositoriesFragment()
 
     }
 
-    private fun navigateRepositoriesFragment(){
+    private fun navigateRepositoriesFragment() {
         val repositoriesFragment = RepositoriesListFragment()
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_fragment_container_view, repositoriesFragment)
         transaction.commit()
     }
 
+    // Вызывается при уничтожении экрана
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("MainActivity","Был вызван onDestroy")
+        Log.d("MainActivity", "Был вызван onDestroy")
     }
 
-    override fun onStart(){
+    override fun onStart() {
         super.onStart()
-        Log.d("MainActivity","Был вызван onStart")
+        Log.d("MainActivity", "Был вызван onStart")
     }
 
-    override fun onStop(){
+    override fun onStop() {
         super.onStop()
-        Log.d("MainActivity","Был вызван onStop")
+        Log.d("MainActivity", "Был вызван onStop")
     }
 }
 
